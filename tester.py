056dd35d43cf1a051699c0d846dbc0a97ead90ec
meDracula <me.dracula@protonmail.com>
2022-12-09 10:09:51 +0100

    added function something to tester

--- tester.py
+++ tester.py
@@ -1,11 +1,14 @@
 def main():
-    #COMMENT
+    # COMMENT
     echo("Hello word")
 
 
 def echo(sentence: str):
     print(sentence)
 
+
+def something(arg) -> str:
+    return "hello world"
 
 if __name__ == '__main__':
     main()
